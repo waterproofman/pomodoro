2b869be34d9831f14813389d5afb87f1079272c9
waterproofman <pp.debski@gmail.com>
2017-10-26 15:07:25 +0200

    Timer set to 25 minutes

--- pomodoro.cpp
+++ pomodoro.cpp
@@ -5,7 +5,7 @@
 int main(void)
 {
 using namespace std::chrono_literals;
-    std::this_thread::sleep_for(5s);
+    std::this_thread::sleep_for(20min);
     for(int i = 0; i < 50; i++)
     {
         for(int j = 0; j < 5; j++)
