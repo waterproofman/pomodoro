973ab70f27997c02c43ff3f6b16966ac41e336b3
waterproofman <pp.debski@gmail.com>
2017-10-26 23:39:31 +0200

    time modified, starting print added.

--- pomodoro.cpp
+++ pomodoro.cpp
@@ -5,11 +5,12 @@
 int main(void)
 {
 using namespace std::chrono_literals;
-    std::this_thread::sleep_for(20min);
+    std::cout<<"Pomodoro counter started!"<<std::endl;
+    std::this_thread::sleep_for(25min);
     for(int i = 0; i < 50; i++)
     {
         for(int j = 0; j < 5; j++)
-            std::cout<<"POMODORO OVER     ";
+            std::cout<<"POMODORO OVER    ";
         std::cout<<std::endl;
     }
     return 0;
